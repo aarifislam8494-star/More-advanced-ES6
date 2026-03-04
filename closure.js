1026195d248cff0011377ccd9ce15d16aeea28c7
aarifislam8494-star <aarifislam8494@gmail.com>
2026-03-04 17:29:28 +0600

    (Advanced) Explore closure in details

---
+++ closure.js
@@ -0,0 +1,39 @@
+function outerFunction(){
+  function innerFunction(){
+   console.log('This is the inner function');
+  }
+  return innerFunction;
+}
+
+const result  = outerFunction();
+// result();
+// console.log('in the outside', result);
+
+
+function counter(owner){
+ let count = 0;
+ function increment(){
+  count = count + 1;
+  console.log('value of count', owner, count);
+ }
+ return increment;
+}
+
+const rahimCounter = counter('Rahim');
+rahimCounter();
+rahimCounter();
+rahimCounter();
+rahimCounter();
+
+const karimCounnter = counter('karim');
+karimCounnter();
+karimCounnter();
+rahimCounter();
+rahimCounter();
+
+// const count1 = counter();
+// console.log(count1);
+// count1();
+// count1();
+// count1();
+// count1();
