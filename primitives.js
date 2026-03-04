cdde6658b6f5978ed41bebbdb250a8e2abf2764a
aarifislam8494-star <aarifislam8494@gmail.com>
2026-03-04 14:41:40 +0600

    primitive and non-primitive data types

---
+++ primitives.js
@@ -0,0 +1,20 @@
+let data = 4;
+
+data = 'Tormuj';
+data = false;
+data = {price: 22}
+data = []
+// console.log(typeof data);
+
+//primitive data type
+let a = 4;
+let b = 'tormuj';
+let c = false;
+
+//non primitive data types
+let d = {price: 22}
+let e = [1, 2, 3, 4]
+
+
+
+
